caa2f6d72b55bd370713637fbbbe220f3badbef8
Vil <101118033+villeneuveandhira@users.noreply.github.com>
2023-06-10 11:06:56 +0700

    Update view_table.php

--- views/view_table.php
+++ views/view_table.php
@@ -12,7 +12,7 @@
     <div class="showstab">
         <!-- Back -->
         <br>
-        <a href="http://localhost/uts-web/" class="back">Back to Home</a>
+        <a href="http://localhost/kuliah/designweb/uts-web/" class="back">Back to Home</a>
         <a href="search.php" class="back">REFRESH</a>
         <a href="user.php" class="back">REGISTER</a>
         <br><br><br>
